921dafbf0bda112f60fff1ac1c5d791ea31546de
Siarhei <siarhei.by@gmail.com>
2022-12-26 19:56:13 +0300

    feat: setup eslint and prettier

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,37 +1,41 @@
 import { useEffect, useState } from 'react';
 import { db } from '../firebase';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs } from 'firebase/firestore';
 import './App.css';
 
 function App() {
   const [todos, setTodods] = useState([]);
 
   useEffect(() => {
-    getDocs(collection(db, "todos"))
+    getDocs(collection(db, 'todos'))
       .then((querySnapshot) => {
         console.log('querySnapshot', querySnapshot);
-        const dbTodos = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+        const dbTodos = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
         console.log('dbTodos', dbTodos);
         setTodods(dbTodos);
       })
-      .catch(err => console.log(err));
-
-    //   db.collection("todos")
-    //   .get()
-    //   .then((querySnapshot) => {
-    //     console.log('snapshot', querySnapshot);
-    //     const dbTodos = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data}));
-    //     console.log('dbTodos', dbTodos);
-    //     setTodods(dbTodos);
-    // });
+      .catch((err) => console.log(err));
   }, []);
+
+  const collections = [{ title: 'home' }, { title: 'planed' }, { title: 'important' }];
 
   return (
     <div className="App">
-      <h1>To-do app</h1>
-      <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
+      <section>
+        <h1>To-do app</h1>
+        <ul>
+          {collections.map((list, i) => (
+            <li key={i}>{list.title}</li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVIesZAecn5L-4mnmhBxCV251tcXZ8lFU",
-  authDomain: "react-to-do-app-207e9.firebaseapp.com",
-  projectId: "react-to-do-app-207e9",
-  storageBucket: "react-to-do-app-207e9.appspot.com",
-  messagingSenderId: "777811654217",
-  appId: "1:777811654217:web:21f3ecfb8b29cf0e97f198"
+  apiKey: 'AIzaSyBVIesZAecn5L-4mnmhBxCV251tcXZ8lFU',
+  authDomain: 'react-to-do-app-207e9.firebaseapp.com',
+  projectId: 'react-to-do-app-207e9',
+  storageBucket: 'react-to-do-app-207e9.appspot.com',
+  messagingSenderId: '777811654217',
+  appId: '1:777811654217:web:21f3ecfb8b29cf0e97f198',
 };
 
 const app = initializeApp(firebaseConfig);
