566e1ca456e0601103f992f59432478bdbaf635d
Siarhei <siarhei.by@gmail.com>
2023-01-28 16:11:16 +0300

    refactor: style NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,11 +1,17 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../components';
+import NotFoundImg from '../assets/img/404.png';
 import * as ROUTES from '../constants/routes';
 
 export function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
-    <div className="p-4 flex flex-col items-center gap-4">
-      <h1 className="text-2xl">Not found page</h1>
-      <Link to={ROUTES.MAIN}>Back to main page</Link>
+    <div className="h-screen p-4 flex flex-col justify-center items-center gap-4">
+      <img className="w-96 object-contain" src={NotFoundImg} alt="error image" />
+      <h1 className="text-2xl text-center font-bold uppercase">Page not found!</h1>
+      <p className="text-xl text-center">Sorry, we can&lsquo;t find the page you requested.</p>
+      <Button onClick={() => navigate(ROUTES.LANDING)}>Home</Button>
     </div>
   );
 }
